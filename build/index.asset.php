69e194e60e8ca52110144da27bcdf53d094decca
Jen Wachter <jwachter@jhu.edu>
2024-03-15 12:14:39 -0400

    Build library

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '51a2d75cfc6f3d4e3ea7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '4b05077ba44fc98be20d');
